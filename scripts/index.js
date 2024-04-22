b89c67571d4b122884b41f95cb3ff93fcf4d60d6
Evgenia Loshakova <janelo1984@yandex.ru>
2024-04-22 21:39:16 +0300

    First-1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,33 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+const cardInfo = initialCards.map(function (item) {
+return {
+  name: item.name,
+  link: item.link
+};
+});
+
+function render() {
+cardInfo.forEach(renderCard);
+}
+
+function renderCard({ name, link }) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__title').textContent = name;
+  placesContainer.append(cardElement);
+}
+
+render();
+
+const deleteButton = document.querySelector('.card__delete-button');
+deleteButton.addEventListener('click', function () {
+  const listItem = deleteButton.closest('.places__item');
+  listItem.remove();
+});
+
